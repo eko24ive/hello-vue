f68b2f1e85196f21f57f6baff21100896317b0de
Erik Kotsiuba <eko24ive@gmail.com>
2018-12-13 01:46:20 +0200

    strikethrough support for todo item

--- src/components/TodoItem/index.vue
+++ src/components/TodoItem/index.vue
@@ -2,10 +2,17 @@
   <div class="todo-item">
     <input type="checkbox" v-model="todo.done" @click="toggleTodo(todo.id)">
     <div class="todo-text-input-wrapper" @dblclick="setEdting(true)">
-      <span class="text" v-if="!editing">
+      <span class="text" v-if="!editing" :class="{ 'strikethrough': todo.done }">
         {{todo.text}}
       </span>
-      <input type="text" class="todo-edit" v-if="editing" :value="todo.text" @keyup.enter="editTodoHandle" @keyup.esc="setEdting(false)"/>
+      <input
+        type="text"
+        class="todo-edit"
+        v-if="editing"
+        :value="todo.text"
+        @keyup.enter="editTodoHandle"
+        @keyup.esc="setEdting(false)"
+      />
     </div>
     <button @click="removeTodo(todo.id)">x</button>
   </div>
