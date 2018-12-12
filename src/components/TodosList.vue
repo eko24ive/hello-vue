978346b1e440e01a9710be36d3a32a6586b505f5
Erik <eko24ive@gmail.com>
2018-12-12 16:18:22 +0200

    implemented TodoList

--- src/components/TodosList.vue
+++ src/components/TodosList.vue
@@ -1,9 +1,34 @@
 <template>
-  <h1>TodoList</h1>
+  <div class="todo-list">
+    <div v-for="todo in todos" v-bind:key="todo.id">
+      <todo-item :todo="todo"/>
+    </div>
+  </div>
 </template>
 
 <script>
+import {mapGetters} from 'vuex';
+
+import TodoItem from './TodoItem';
+
 export default {
   name: 'TodoList',
+  components: {
+    TodoItem
+  },
+  data: function (){
+    return {}
+  },
+  computed: {
+    ...mapGetters('todos', {
+      todos: 'all'
+    })
+  }
 }
 </script>
+
+<style scoped>
+  .todo-list {
+    margin-top: 15px;
+  }
+</style>
