89da704b4e5d8d2c379ac59bd885795267fd34ac
Erik Kotsiuba <eko24ive@gmail.com>
2018-12-12 02:19:49 +0200

    created home page

---
+++ src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      This is home page
+      <TodoInput/>
+    </div>
+</template>
+
+<script>
+  import TodoInput from '../components/TodoInput.vue';
+
+  export default {
+    name: 'Home',
+    components: {
+      TodoInput
+    }
+  }
+</script>
