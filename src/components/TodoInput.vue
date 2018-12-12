fef76d23bf5d6eb90da333daae47d552806c115e
Erik <eko24ive@gmail.com>
2018-12-12 16:18:05 +0200

    styling

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <input type="text" v-on:keypress="handleKeypress">
+    <input type="text" v-on:keypress="handleKeypress" placeholder="What needs to be done?">
   </div>
 </template>
 
 <script>
-  import {mapActions, mapGetters} from 'vuex';
+  import {mapActions} from 'vuex';
 
   export default {
     name: 'TodoInput',
@@ -25,3 +25,13 @@
     }
   }
 </script>
+
+<style scoped>
+  input {
+    width: 100%;
+    font-size: 24px;
+    padding: 5px;
+    border: 0;
+    box-shadow: 2px -2px 8px 1px rgba(0, 0, 0, 0.13);
+  }
+</style>
