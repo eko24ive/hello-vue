5cf6248f3ed038e4490f04a4e731044d342199f1
Erik Kotsiuba <eko24ive@gmail.com>
2018-12-12 02:20:48 +0200

    created todo input

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input type="text" v-on:keypress="handleKeypress">
+  </div>
+</template>
+
+<script>
+  import {mapActions, mapGetters} from 'vuex';
+
+  export default {
+    name: 'TodoInput',
+    methods: {
+      ...mapActions('todos', [
+        'addTodo',
+      ]),
+      handleKeypress(e) {
+        if(e.keyCode === 13) {
+          this.addTodo({
+            text: e.target.value,
+            done: false
+          })
+          e.target.value = "";
+        }
+      }
+    }
+  }
+</script>
