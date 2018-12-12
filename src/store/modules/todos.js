f2d82d1f68c20a334b3a82269b91a7ac8d63db4e
Erik <eko24ive@gmail.com>
2018-12-12 16:19:10 +0200

    adding todos with id; added remove action;

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -5,18 +5,32 @@ const state = {
 const mutations = {
   addTodo(state, todo) {
     state.todos.push(todo);
+  },
+  removeTodo(state, id) {
+    const todoIndex = state.todos.findIndex(todo => todo.id === id);
+
+    state.todos = [...state.todos.slice(0, todoIndex), ...state.todos.slice(todoIndex + 1)]
   }
 };
 
 const actions = {
-  addTodo({ commit }, todo) {
-    commit('addTodo', todo);
+  addTodo({ state, commit }, todo) {
+    commit('addTodo', {
+      ...todo,
+      id: state.todos.length
+    });
+  },
+  removeTodo({ commit }, id) {
+    commit('removeTodo', id)
   }
 };
 
 const getters = {
   all(state) {
     return state.todos;
+  },
+  active(state) {
+    return state.todos.filter(todo => !todo.done);
   },
   completed(state) {
     return state.todos.filter(todo => todo.done);
