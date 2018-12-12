8c0421777a044b09b5ed771a506ead2cb22cebf5
Erik <eko24ive@gmail.com>
2018-12-12 16:18:33 +0200

    implemented TodoItem with deleting

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,9 +1,43 @@
 <template>
-  <h1>TodoItem</h1>
+  <div class="todo-item">
+    <input type="checkbox">
+    <div class="todo-text">
+      {{todo.text}}
+    </div>
+    <button v-on:click="handleRemove">x</button>
+  </div>
 </template>
 
 <script>
+import {mapActions} from 'vuex';
+
 export default {
   name: 'TodoItem',
+  props: {
+    todo: {
+      type: Object
+    }
+  },
+  methods: {
+    ...mapActions('todos', [
+      'removeTodo',
+    ]),
+    handleRemove() {
+      this.removeTodo(this.todo.id)
+    }
+  }
 }
+
+
 </script>
+
+<style scoped>
+  .todo-item {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .todo-text {
+    flex: 1;
+  }
+</style>
